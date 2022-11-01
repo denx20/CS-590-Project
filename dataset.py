a699d02d2bcf1fbb2c61aeeab9cd1693fc0399ec
Yuxi Long <longyuxi@live.com>
2022-11-01 15:38:32 -0400

    wrote dataloader

---
+++ dataset.py
@@ -0,0 +1,74 @@
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import Dataset, DataLoader
+import sequence_generator
+from typing import Optional
+
+# Dataset: Spits out sequence with whether possible_terms exist
+class FunctionDataset(Dataset):
+    def __init__(self,
+        size=100, generate_on_call=False,
+        additional_function_maker_params=()) -> None:
+        """If generate on call, we randomly make a function every time this thing
+        is called, which will probably give different functions across
+        different epochs.
+
+        Otherwise, generate all the functions at init.
+
+        Args:
+            size (int, optional): _description_. Defaults to 1000.
+            generate_on_call (bool, optional): _description_. Defaults to False.
+            additional_function_maker_params (tuple, optional): _description_. Defaults to ().
+        """
+
+        super().__init__()
+        self.size = size
+        self.additional_function_maker_params = additional_function_maker_params
+        self.num_possible_terms = len(sequence_generator.make_possible_terms())
+
+        self.generate_on_call = generate_on_call
+        self.data = []
+
+        if not self.generate_on_call:
+            for i in range(self.size):
+                f, seq, terms = sequence_generator.make_random_function()
+                seq = torch.tensor(seq, dtype=torch.float)
+                terms = torch.tensor(terms, dtype=torch.float)
+                self.data.append((seq, terms))
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __getitem__(self, idx):
+        if self.generate_on_call:
+            # We don't need to use index here because everything is randomly generated
+            f, seq, terms = sequence_generator.make_random_function()
+            seq = torch.tensor(seq, dtype=torch.float)
+            terms = torch.tensor(terms, dtype=torch.float)
+            return seq, terms
+        else:
+            return self.data[idx]
+
+class FunctionDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size: int = 32, num_workers=8):
+        super().__init__()
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        self.train_set = FunctionDataset(size=3000)
+        self.val_set = FunctionDataset(size=300)
+        self.test_set = FunctionDataset(size=300)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_set, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_set, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_set, batch_size=self.batch_size, num_workers=self.num_workers)
+
+if __name__ == '__main__':
+    f = FunctionDataset(size=1000)
+    print(f[0])
--- sequence_generator.py
+++ sequence_generator.py
@@ -2,6 +2,7 @@
 
 from function import Function, FunctionTerm
 import random
+import numpy as np
 
 def make_possible_terms():
     """Generates a collection of all the possible individual terms. When generating functions, we randomly select terms from this list and assign random integer coefficients to it.
@@ -34,13 +35,39 @@ def make_possible_terms():
 
     return possible_terms
 
+def make_random_function(sequence_bound=10000, nterms=5, coefficient_range=(1, 5)):
+    # Find a function with max(sequence) <= sequence_bound
+    # Returns (function, sequence, terms_used)
+    # terms_used is a boolean array
 
+    possible_terms = make_possible_terms() # Make a copy of this list every iteration
+
+    while True: # this process sometimes fails
+        f = Function()
+        term_choices = random.sample(range(len(possible_terms)), k=nterms)
+        for term_index in term_choices:
+            t = possible_terms[term_index]
+            t.updateCoeff(random.randint(*coefficient_range)) # randomly assign coefficient
+            f.addTerm(t)
+
+        sequence = make_sequence(f)
+        if max(sequence) > sequence_bound: continue
+        else:
+            boolean_term_mask = np.zeros(len(possible_terms), dtype=bool)
+            boolean_term_mask[term_choices] = True
+            return f, sequence, boolean_term_mask
+
+
+### DEPRECATED: USE make_random_function() INSTEAD
 def make_functions(
     num_functions_generated=10,
     num_terms_mean=4, num_terms_stdev=2, min_num_terms=2,
     coefficient_range=(1, 5)
     ):
-    """Returns a list of (functions, terms used) found by concatenating terms given by make_possible_terms(). The number of terms is drawn from a Gaussian distribution.
+    """
+    DEPRECATED: USE make_random_function() INSTEAD
+
+    Returns a list of (functions, terms used) found by concatenating terms given by make_possible_terms(). The number of terms is drawn from a Gaussian distribution.
 
     terms_used is expressed as a list of indices to the output of make_possible_terms().
 
@@ -52,9 +79,9 @@ def make_functions(
         coefficient_range (tuple, optional): Range of coefficients before each term. Defaults to (1, 5).
     """
 
-    print(f'Params passed to make_functions(): {locals()}')
-    possible_terms = make_possible_terms()
-    print(f'Possible terms: {possible_terms}')
+    # print(f'Params passed to make_functions(): {locals()}')
+    # possible_terms = make_possible_terms()
+    # print(f'Possible terms: {possible_terms}')
 
     out = [] # the list of functions to return
     while len(out) != num_functions_generated:
@@ -130,9 +157,10 @@ def run():
     return seqs
   
 if __name__ == '__main__':
-    f = Function()
-    f.addTerm(FunctionTerm(type='constant', c=2))
-    f.addTerm(FunctionTerm(type='loc_term', c=1, exponent1=1, index_diff1=1))
-    print(f)
-    print(make_sequence(f))
-    # run()
+    # f = Function()
+    # f.addTerm(FunctionTerm(type='constant', c=2))
+    # f.addTerm(FunctionTerm(type='loc_term', c=1, exponent1=1, index_diff1=1))
+    # print(f)
+    # print(make_sequence(f))
+    # # run()
+    print(make_random_function())
